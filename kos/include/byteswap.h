b03f4d7b73ca21ea4396521165f18b2f19fcb02d
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2020-01-20 11:33:01 +0100

    Cleanup

--- kos/include/byteswap.h
+++ kos/include/byteswap.h
@@ -21,10 +21,11 @@
 #define _BYTESWAP_H 1
 
 #include <features.h>
+
 #include <bits/byteswap.h>
 
-#define bswap_16(x)   __bswap_16(x)
-#define bswap_32(x)   __bswap_32(x)
-#define bswap_64(x)   __bswap_64(x)
+#define bswap_16(x) __bswap_16(x)
+#define bswap_32(x) __bswap_32(x)
+#define bswap_64(x) __bswap_64(x)
 
 #endif /* !_BYTESWAP_H */
