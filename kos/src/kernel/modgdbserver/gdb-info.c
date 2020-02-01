c83ca387e11b8f905d2d8135f209debbf302f96e
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2020-02-01 21:05:02 +0100

    Implement xml support for gdbserver on x86_64
    Note however that this doesn't (yet?) include multi-arch support. - gdbserver on x86_64 only known about 64-bit code at the moment...

--- kos/src/kernel/modgdbserver/gdb-info.c
+++ kos/src/kernel/modgdbserver/gdb-info.c
@@ -936,6 +936,30 @@ NOTHROW(FCALL GDBInfo_PrintOSData)(pformatprinter printer, void *arg,
 	"<!DOCTYPE feature SYSTEM \"gdb-target.dtd\">"
 
 #ifdef __x86_64__
+#define HAVE_GDBFeatures_target_xml 1
+#define HAVE_GDBFeatures_64bit_sse_xml 1
+#define HAVE_GDBFeatures_64bit_core_xml 1
+#define HAVE_GDBFeatures_64bit_segments_xml 1
+#define PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN \
+	COMPILER_STRLEN(XML_VERSION_HEADER GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD)
+#define PRINT_GDBFeatures_target_xml()                                                                    \
+	PRINT(GDBFeatures_target_xml1);                                                                       \
+	print(GDBFeatures_64bit_core_xml + PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN,                       \
+	      COMPILER_STRLEN(GDBFeatures_64bit_core_xml) - PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN);     \
+	print(GDBFeatures_64bit_sse_xml + PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN,                        \
+	      COMPILER_STRLEN(GDBFeatures_64bit_sse_xml) - PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN);      \
+	print(GDBFeatures_64bit_segments_xml + PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN,                   \
+	      COMPILER_STRLEN(GDBFeatures_64bit_segments_xml) - PRIVATE_GDB_TARGETX86_64_SUBFILE_PREFIX_LEN); \
+	PRINT(GDBFeatures_target_xml2);
+PRIVATE char const GDBFeatures_target_xml1[] =
+XML_VERSION_HEADER
+"<!DOCTYPE target SYSTEM \"gdb-target.dtd\">"
+"<target>"
+	"<architecture>i386:x86-64</architecture>"
+//	"<osabi>KOS</osabi>"
+;
+PRIVATE char const GDBFeatures_target_xml2[] =
+"</target>";
 #elif defined(__i386__)
 #define HAVE_GDBFeatures_target_xml 1
 #define HAVE_GDBFeatures_32bit_sse_xml 1
@@ -1030,8 +1054,7 @@ GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD
 	"<reg name=\"foseg\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
 	"<reg name=\"fooff\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
 	"<reg name=\"fop\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
-"</feature>"
-;
+"</feature>";
 #endif /* HAVE_GDBFeatures_32bit_core_xml */
 
 /* "<xi:include href=\"32bit-sse.xml\"/>" */
@@ -1090,6 +1113,164 @@ GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD
 #endif /* HAVE_GDBFeatures_32bit_sse_xml */
 
 
+/* "<xi:include href=\"64bit-core.xml\"/>" */
+#ifdef HAVE_GDBFeatures_64bit_core_xml
+#define HAVE_GDB_XML 1
+PRIVATE char const GDBFeatures_64bit_core_xml[] =
+XML_VERSION_HEADER
+//"<!-- Copyright (C) 2010-2019 Free Software Foundation, Inc.\n"
+//"\n"
+//"     Copying and distribution of this file, with or without modification,\n"
+//"     are permitted in any medium without royalty provided the copyright\n"
+//"     notice and this notice are preserved.  -->\n"
+GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD
+"<feature name=\"org.gnu.gdb.i386.core\">"
+	"<flags id=\"i386_eflags\" size=\"4\">"
+		"<field name=\"CF\" start=\"0\" end=\"0\"/>"
+		"<field name=\"\" start=\"1\" end=\"1\"/>"
+		"<field name=\"PF\" start=\"2\" end=\"2\"/>"
+		"<field name=\"AF\" start=\"4\" end=\"4\"/>"
+		"<field name=\"ZF\" start=\"6\" end=\"6\"/>"
+		"<field name=\"SF\" start=\"7\" end=\"7\"/>"
+		"<field name=\"TF\" start=\"8\" end=\"8\"/>"
+		"<field name=\"IF\" start=\"9\" end=\"9\"/>"
+		"<field name=\"DF\" start=\"10\" end=\"10\"/>"
+		"<field name=\"OF\" start=\"11\" end=\"11\"/>"
+		"<field name=\"NT\" start=\"14\" end=\"14\"/>"
+		"<field name=\"RF\" start=\"16\" end=\"16\"/>"
+		"<field name=\"VM\" start=\"17\" end=\"17\"/>"
+		"<field name=\"AC\" start=\"18\" end=\"18\"/>"
+		"<field name=\"VIF\" start=\"19\" end=\"19\"/>"
+		"<field name=\"VIP\" start=\"20\" end=\"20\"/>"
+		"<field name=\"ID\" start=\"21\" end=\"21\"/>"
+	"</flags>"
+	"<reg name=\"rax\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rbx\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rcx\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rdx\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rsi\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rdi\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rbp\" bitsize=\"64\" type=\"data_ptr\"/>"
+	"<reg name=\"rsp\" bitsize=\"64\" type=\"data_ptr\"/>"
+	"<reg name=\"r8\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r9\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r10\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r11\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r12\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r13\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r14\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"r15\" bitsize=\"64\" type=\"int64\"/>"
+	"<reg name=\"rip\" bitsize=\"64\" type=\"code_ptr\"/>"
+	"<reg name=\"eflags\" bitsize=\"32\" type=\"i386_eflags\"/>"
+	"<reg name=\"cs\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"ss\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"ds\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"es\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"fs\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"gs\" bitsize=\"32\" type=\"int32\"/>"
+	"<reg name=\"st0\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st1\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st2\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st3\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st4\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st5\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st6\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"st7\" bitsize=\"80\" type=\"i387_ext\"/>"
+	"<reg name=\"fctrl\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"fstat\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"ftag\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"fiseg\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"fioff\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"foseg\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"fooff\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+	"<reg name=\"fop\" bitsize=\"32\" type=\"int\" group=\"float\"/>"
+"</feature>";
+#endif /* HAVE_GDBFeatures_64bit_core_xml */
+
+
+/* "<xi:include href=\"64bit-sse.xml\"/>" */
+#ifdef HAVE_GDBFeatures_64bit_sse_xml
+#define HAVE_GDB_XML 1
+PRIVATE char const GDBFeatures_64bit_sse_xml[] =
+XML_VERSION_HEADER
+//"<!-- Copyright (C) 2010-2019 Free Software Foundation, Inc.\n"
+//"\n"
+//"     Copying and distribution of this file, with or without modification,\n"
+//"     are permitted in any medium without royalty provided the copyright\n"
+//"     notice and this notice are preserved.  -->"
+GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD
+"<feature name=\"org.gnu.gdb.i386.sse\">"
+	"<vector id=\"v4f\" type=\"ieee_single\" count=\"4\"/>"
+	"<vector id=\"v2d\" type=\"ieee_double\" count=\"2\"/>"
+	"<vector id=\"v16i8\" type=\"int8\" count=\"16\"/>"
+	"<vector id=\"v8i16\" type=\"int16\" count=\"8\"/>"
+	"<vector id=\"v4i32\" type=\"int32\" count=\"4\"/>"
+	"<vector id=\"v2i64\" type=\"int64\" count=\"2\"/>"
+	"<union id=\"vec128\">"
+		"<field name=\"v4_float\" type=\"v4f\"/>"
+		"<field name=\"v2_double\" type=\"v2d\"/>"
+		"<field name=\"v16_int8\" type=\"v16i8\"/>"
+		"<field name=\"v8_int16\" type=\"v8i16\"/>"
+		"<field name=\"v4_int32\" type=\"v4i32\"/>"
+		"<field name=\"v2_int64\" type=\"v2i64\"/>"
+		"<field name=\"uint128\" type=\"uint128\"/>"
+	"</union>"
+	"<flags id=\"i386_mxcsr\" size=\"4\">"
+		"<field name=\"IE\" start=\"0\" end=\"0\"/>"
+		"<field name=\"DE\" start=\"1\" end=\"1\"/>"
+		"<field name=\"ZE\" start=\"2\" end=\"2\"/>"
+		"<field name=\"OE\" start=\"3\" end=\"3\"/>"
+		"<field name=\"UE\" start=\"4\" end=\"4\"/>"
+		"<field name=\"PE\" start=\"5\" end=\"5\"/>"
+		"<field name=\"DAZ\" start=\"6\" end=\"6\"/>"
+		"<field name=\"IM\" start=\"7\" end=\"7\"/>"
+		"<field name=\"DM\" start=\"8\" end=\"8\"/>"
+		"<field name=\"ZM\" start=\"9\" end=\"9\"/>"
+		"<field name=\"OM\" start=\"10\" end=\"10\"/>"
+		"<field name=\"UM\" start=\"11\" end=\"11\"/>"
+		"<field name=\"PM\" start=\"12\" end=\"12\"/>"
+		"<field name=\"FZ\" start=\"15\" end=\"15\"/>"
+	"</flags>"
+	"<reg name=\"xmm0\" bitsize=\"128\" type=\"vec128\" regnum=\"40\"/>"
+	"<reg name=\"xmm1\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm2\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm3\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm4\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm5\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm6\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm7\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm8\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm9\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm10\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm11\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm12\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm13\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm14\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"xmm15\" bitsize=\"128\" type=\"vec128\"/>"
+	"<reg name=\"mxcsr\" bitsize=\"32\" type=\"i386_mxcsr\" group=\"vector\"/>"
+"</feature>";
+#endif /* HAVE_GDBFeatures_64bit_sse_xml */
+
+
+/* "<xi:include href=\"64bit-segments.xml\"/>" */
+#ifdef HAVE_GDBFeatures_64bit_segments_xml
+#define HAVE_GDB_XML 1
+PRIVATE char const GDBFeatures_64bit_segments_xml[] =
+XML_VERSION_HEADER
+//"<!-- Copyright (C) 2010-2019 Free Software Foundation, Inc.\n"
+//"\n"
+//"     Copying and distribution of this file, with or without modification,\n"
+//"     are permitted in any medium without royalty provided the copyright\n"
+//"     notice and this notice are preserved.  -->"
+GDB_FEATURES_DOCTYPE_FEATURE_SYSTEM_GDB_TARGET_DTD
+"<feature name=\"org.gnu.gdb.i386.segments\">"
+	"<reg name=\"fs_base\" bitsize=\"64\" type=\"int\"/>"
+	"<reg name=\"gs_base\" bitsize=\"64\" type=\"int\"/>"
+"</feature>";
+#endif /* HAVE_GDBFeatures_64bit_segments_xml */
+
+
+
 #ifdef HAVE_GDBFeatures_target_xml
 #define HAVE_GDB_XML 1
 #endif /* HAVE_GDBFeatures_target_xml */
@@ -1122,6 +1303,21 @@ NOTHROW(FCALL GDBInfo_PrintFeaturesFile)(pformatprinter printer, void *arg,
 		PRINT(GDBFeatures_32bit_core_xml);
 	} else
 #endif /* HAVE_GDBFeatures_32bit_core_xml */
+#ifdef HAVE_GDBFeatures_64bit_sse_xml
+	if (strcmp(filename, "64bit-sse.xml") == 0) {
+		PRINT(GDBFeatures_64bit_sse_xml);
+	} else
+#endif /* HAVE_GDBFeatures_64bit_sse_xml */
+#ifdef HAVE_GDBFeatures_64bit_core_xml
+	if (strcmp(filename, "64bit-core.xml") == 0) {
+		PRINT(GDBFeatures_64bit_core_xml);
+	} else
+#endif /* HAVE_GDBFeatures_64bit_core_xml */
+#ifdef HAVE_GDBFeatures_64bit_segments_xml
+	if (strcmp(filename, "64bit-segments.xml") == 0) {
+		PRINT(GDBFeatures_64bit_segments_xml);
+	} else
+#endif /* HAVE_GDBFeatures_64bit_segments_xml */
 	{
 		result = -ENOENT;
 	}
