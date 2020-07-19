afae7cad634a02b98973c136114366e98866cbfe
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2020-07-19 12:04:54 +0200

    Disable ansitty debug logging
    Stuff written to ttys already gets logged as part of sctrace in the form of `task[<pid>].sys_write(...)' trace messages. - So enabling this is pretty much just bloat.

--- kos/src/kernel/core/dev/ansitty.c
+++ kos/src/kernel/core/dev/ansitty.c
@@ -46,7 +46,7 @@ ansitty_device_write(struct character_device *__restrict self,
                      USER CHECKED void const *src,
                      size_t num_bytes, iomode_t UNUSED(mode)) THROWS(...) {
 	struct ansitty_device *me = (struct ansitty_device *)self;
-#if !defined(NDEBUG) && 1
+#if !defined(NDEBUG) && 0
 	printk(KERN_DEBUG "[ansitty_device_write] %$q\n", num_bytes, src);
 #endif
 	return (size_t)ansitty_printer(&me->at_ansi, (char const *)src, num_bytes);
