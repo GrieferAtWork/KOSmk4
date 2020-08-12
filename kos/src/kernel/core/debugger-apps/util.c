321ee9ccc2e111c3fff91ac46f5ca4c7f93c8988
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2020-08-12 14:31:54 +0200

    Add a new debugger command `sync'
    Does the same as the commandline `sync' program: Flush unwritten changes to disk.

---
+++ kos/src/kernel/core/debugger-apps/util.c
@@ -0,0 +1,52 @@
+/*[[[magic
+local gcc_opt = options.setdefault("GCC.options", []);
+if (gcc_opt.removeif([](x) -> x.startswith("-O")))
+	gcc_opt.append("-Os");
+]]]*/
+/* Copyright (c) 2019-2020 Griefer@Work                                       *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement (see the following) in the product     *
+ *    documentation is required:                                              *
+ *    Portions Copyright (c) 2019-2020 Griefer@Work                           *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ */
+#ifndef GUARD_KERNEL_SRC_DEBUGGER_APPS_UTIL_C
+#define GUARD_KERNEL_SRC_DEBUGGER_APPS_UTIL_C 1
+#define DISABLE_BRANCH_PROFILING 1
+
+#include <kernel/compiler.h>
+
+#include <debugger/config.h>
+#ifdef CONFIG_HAVE_DEBUGGER
+#include <debugger/debugger.h>
+#include <fs/node.h>
+
+DECL_BEGIN
+
+DBG_COMMAND(sync,
+            "sync\n"
+            "\tSynchronize filesystem/disk modifications\n",
+            argc, argv) {
+	(void)argv;
+	if (argc != 1)
+		return DBG_STATUS_INVALID_ARGUMENTS;
+	superblock_syncall();
+	return 0;
+}
+
+DECL_END
+#endif /* CONFIG_HAVE_DEBUGGER */
+
+#endif /* !GUARD_KERNEL_SRC_DEBUGGER_APPS_UTIL_C */
