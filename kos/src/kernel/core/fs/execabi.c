f77764375a273b2102827973ecf809da1f4f2fdc
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2020-10-15 18:09:42 +0200

    Fix argument injection by shebang

--- kos/src/kernel/core/fs/execabi.c
+++ kos/src/kernel/core/fs/execabi.c
@@ -86,7 +86,7 @@ NOTHROW(FCALL execabis_destroy)(struct execabis_struct *__restrict self) {
 /* Finalize the given set of exec() arguments. */
 PUBLIC NOBLOCK NONNULL((1)) void
 NOTHROW(FCALL execargs_fini)(struct execargs *__restrict self) {
-	if unlikely(self->ea_argc_inject) {
+	if unlikely(self->ea_argv_inject) {
 		size_t i;
 		for (i = 0; i < self->ea_argc_inject; ++i)
 			kfree(self->ea_argv_inject[i]);
--- kos/src/kernel/modshebang/shebang.c
+++ kos/src/kernel/modshebang/shebang.c
@@ -205,6 +205,7 @@ shebang_exec(struct execargs *__restrict args) {
 				args->ea_argv_inject[0] = inject_arg0;
 				if (inject_arg1)
 					args->ea_argv_inject[1] = inject_arg1;
+				args->ea_argc_inject += more_argc;
 			} EXCEPT {
 				decref_unlikely(interp_path);
 				decref_unlikely(interp_dentry);
