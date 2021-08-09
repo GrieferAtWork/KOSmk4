e56dc5d0dc939f0c10942d4372a4a370b0f075e2
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2021-08-09 16:42:42 +0200

    Fix /* in comment

--- kos/include/i386-kos/asm/cpu-cpuid.h
+++ kos/include/i386-kos/asm/cpu-cpuid.h
@@ -51,7 +51,7 @@
 #define CPUID_1D_MCE             __UINT32_C(0x00000080) /* [bit(EDX:7)] Machine Check Exception. */
 #define CPUID_1D_CX8             __UINT32_C(0x00000100) /* [bit(EDX:8)] CMPXCHG8B (compare-and-swap) instruction. */
 #define CPUID_1D_APIC            __UINT32_C(0x00000200) /* [bit(EDX:9)] Onboard Advanced Programmable Interrupt Controller. */
-/*      CPUID_1D_                __UINT32_C(0x00000400) /* [bit(EDX:10)] RESERVED. */
+/*      CPUID_1D_                __UINT32_C(0x00000400)  * [bit(EDX:10)] RESERVED. */
 #define CPUID_1D_SEP             __UINT32_C(0x00000800) /* [bit(EDX:11)] SYSENTER and SYSEXIT instructions. */
 #define CPUID_1D_MTRR            __UINT32_C(0x00001000) /* [bit(EDX:12)] Memory Type Range Registers. */
 #define CPUID_1D_PGE             __UINT32_C(0x00002000) /* [bit(EDX:13)] Page Global Enable bit in CR4. */
@@ -61,7 +61,7 @@
 #define CPUID_1D_PSE36           __UINT32_C(0x00020000) /* [bit(EDX:17)] 36-bit page size extension. */
 #define CPUID_1D_PSN             __UINT32_C(0x00040000) /* [bit(EDX:18)] Processor Serial Number. */
 #define CPUID_1D_CLFSH           __UINT32_C(0x00080000) /* [bit(EDX:19)] CLFLUSH instruction (SSE2). */
-/*      CPUID_1D_                __UINT32_C(0x00100000) /* [bit(EDX:20)] RESERVED. */
+/*      CPUID_1D_                __UINT32_C(0x00100000)  * [bit(EDX:20)] RESERVED. */
 #define CPUID_1D_DS              __UINT32_C(0x00200000) /* [bit(EDX:21)] Debug store: save trace of executed jumps. */
 #define CPUID_1D_ACPI            __UINT32_C(0x00400000) /* [bit(EDX:22)] Onboard thermal control MSRs for ACPI. */
 #define CPUID_1D_MMX             __UINT32_C(0x00800000) /* [bit(EDX:23)] MMX instructions. */
@@ -89,7 +89,7 @@
 #define CPUID_1C_CX16            __UINT32_C(0x00002000) /* [bit(ECX:13)] CMPXCHG16B instruction. */
 #define CPUID_1C_XTPR            __UINT32_C(0x00004000) /* [bit(ECX:14)] Can disable sending task priority messages. */
 #define CPUID_1C_PDCM            __UINT32_C(0x00008000) /* [bit(ECX:15)] Perfmon & debug capability. */
-/*      CPUID_1C_                __UINT32_C(0x00010000) /* [bit(ECX:16)] RESERVED. */
+/*      CPUID_1C_                __UINT32_C(0x00010000)  * [bit(ECX:16)] RESERVED. */
 #define CPUID_1C_PCID            __UINT32_C(0x00020000) /* [bit(ECX:17)] Process context identifiers (CR4 bit 17). */
 #define CPUID_1C_DCA             __UINT32_C(0x00040000) /* [bit(ECX:18)] Direct cache access for DMA writes[12][13]. */
 #define CPUID_1C_SSE4_1          __UINT32_C(0x00080000) /* [bit(ECX:19)] SSE4.1 instructions. */
@@ -106,74 +106,74 @@
 #define CPUID_1C_RDRAND          __UINT32_C(0x40000000) /* [bit(ECX:30)] RDRAND (on-chip random number generator) support. */
 #define CPUID_1C_HYPERVISOR      __UINT32_C(0x80000000) /* [bit(ECX:31)] Running on a hypervisor (always 0 on a real CPU, but also with some hypervisors). */
 
-/*      CPUID_7D_                __UINT32_C(0x00000001) /* [bit(0)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000002) /* [bit(1)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000001)  * [bit(0)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000002)  * [bit(1)] RESERVED. */
 #define CPUID_7D_AVX512_4VNNIW   __UINT32_C(0x00000004) /* [bit(2)] AVX-512 4-register Neural Network Instructions. */
 #define CPUID_7D_AVX512_4FMAPS   __UINT32_C(0x00000008) /* [bit(3)] AVX-512 4-register Multiply Accumulation Single precision. */
-/*      CPUID_7D_                __UINT32_C(0x00000010) /* [bit(4)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000020) /* [bit(5)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000040) /* [bit(6)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000080) /* [bit(7)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000100) /* [bit(8)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000200) /* [bit(9)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000400) /* [bit(10)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00000800) /* [bit(11)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00001000) /* [bit(12)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00002000) /* [bit(13)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00004000) /* [bit(14)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00008000) /* [bit(15)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00010000) /* [bit(16)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00020000) /* [bit(17)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00040000) /* [bit(18)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00080000) /* [bit(19)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00100000) /* [bit(20)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00200000) /* [bit(21)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00400000) /* [bit(22)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x00800000) /* [bit(23)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x01000000) /* [bit(24)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x02000000) /* [bit(25)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x04000000) /* [bit(26)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x08000000) /* [bit(27)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x10000000) /* [bit(28)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x20000000) /* [bit(29)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x40000000) /* [bit(30)] RESERVED. */
-/*      CPUID_7D_                __UINT32_C(0x80000000) /* [bit(31)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000010)  * [bit(4)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000020)  * [bit(5)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000040)  * [bit(6)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000080)  * [bit(7)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000100)  * [bit(8)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000200)  * [bit(9)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000400)  * [bit(10)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00000800)  * [bit(11)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00001000)  * [bit(12)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00002000)  * [bit(13)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00004000)  * [bit(14)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00008000)  * [bit(15)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00010000)  * [bit(16)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00020000)  * [bit(17)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00040000)  * [bit(18)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00080000)  * [bit(19)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00100000)  * [bit(20)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00200000)  * [bit(21)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00400000)  * [bit(22)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x00800000)  * [bit(23)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x01000000)  * [bit(24)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x02000000)  * [bit(25)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x04000000)  * [bit(26)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x08000000)  * [bit(27)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x10000000)  * [bit(28)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x20000000)  * [bit(29)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x40000000)  * [bit(30)] RESERVED. */
+/*      CPUID_7D_                __UINT32_C(0x80000000)  * [bit(31)] RESERVED. */
 #define CPUID_7C_PREFETCHWT1     __UINT32_C(0x00000001) /* [bit(0)] PREFETCHWT1 instruction. */
 #define CPUID_7C_AVX512VBMI      __UINT32_C(0x00000002) /* [bit(1)] AVX-512 Vector Bit Manipulation Instructions. */
 #define CPUID_7C_UMIP            __UINT32_C(0x00000004) /* [bit(2)] User-mode Instruction Prevention. */
 #define CPUID_7C_PKU             __UINT32_C(0x00000008) /* [bit(3)] Memory Protection Keys for User-mode pages. */
 #define CPUID_7C_OSPKE           __UINT32_C(0x00000010) /* [bit(4)] PKU enabled by OS. */
-/*      CPUID_7C_                __UINT32_C(0x00000020) /* [bit(5)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00000020)  * [bit(5)] RESERVED. */
 #define CPUID_7C_AVX512VBMI2     __UINT32_C(0x00000040) /* [bit(6)] AVX-512 Vector Bit Manipulation Instructions 2. */
-/*      CPUID_7C_                __UINT32_C(0x00000080) /* [bit(7)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00000080)  * [bit(7)] RESERVED. */
 #define CPUID_7C_GFNI            __UINT32_C(0x00000100) /* [bit(8)] Galois Field instructions. */
 #define CPUID_7C_VAES            __UINT32_C(0x00000200) /* [bit(9)] AES instruction set (VEX-256/EVEX). */
 #define CPUID_7C_VPCLMULQDQ      __UINT32_C(0x00000400) /* [bit(10)] CLMUL instruction set (VEX-256/EVEX). */
 #define CPUID_7C_AVX512VNNI      __UINT32_C(0x00000800) /* [bit(11)] AVX-512 Vector Neural Network Instructions. */
 #define CPUID_7C_AVX512BITALG    __UINT32_C(0x00001000) /* [bit(12)] AVX-512 BITALG instructions. */
-/*      CPUID_7C_                __UINT32_C(0x00002000) /* [bit(13)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00002000)  * [bit(13)] RESERVED. */
 #define CPUID_7C_AVX512VPOPCNTDQ __UINT32_C(0x00004000) /* [bit(14)] AVX-512 Vector Population Count D/Q. */
-/*      CPUID_7C_                __UINT32_C(0x00008000) /* [bit(15)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x00010000) /* [bit(16)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00008000)  * [bit(15)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00010000)  * [bit(16)] RESERVED. */
 #define CPUID_7C_MAWAU           __UINT32_C(0x003e0000) /* [bit(17...21)] The value of userspace MPX Address-Width Adjust used by
                                  __UINT32_C(          )  *                the BNDLDX and BNDSTX Intel MPX instructions in 64-bit mode. */
 #define CPUID_7C_RDPID           __UINT32_C(0x00400000) /* [bit(22)] Read Processor ID. */
-/*      CPUID_7C_                __UINT32_C(0x00800000) /* [bit(23)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x01000000) /* [bit(24)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x02000000) /* [bit(25)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x04000000) /* [bit(26)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x08000000) /* [bit(27)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x10000000) /* [bit(28)] RESERVED. */
-/*      CPUID_7C_                __UINT32_C(0x20000000) /* [bit(29)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x00800000)  * [bit(23)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x01000000)  * [bit(24)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x02000000)  * [bit(25)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x04000000)  * [bit(26)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x08000000)  * [bit(27)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x10000000)  * [bit(28)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x20000000)  * [bit(29)] RESERVED. */
 #define CPUID_7C_SGX_LC          __UINT32_C(0x40000000) /* [bit(30)] SGX Launch Configuration. */
-/*      CPUID_7C_                __UINT32_C(0x80000000) /* [bit(31)] RESERVED. */
+/*      CPUID_7C_                __UINT32_C(0x80000000)  * [bit(31)] RESERVED. */
 #define CPUID_7B_FSGSBASE        __UINT32_C(0x00000001) /* [bit(0)] Access to base of %fs and %gs. */
 #define CPUID_7B_IA32_TSC_ADJUST __UINT32_C(0x00000002) /* [bit(1)] IA32_TSC_ADJUST. */
 #define CPUID_7B_SGX             __UINT32_C(0x00000004) /* [bit(2)] Software Guard Extensions. */
 #define CPUID_7B_BMI1            __UINT32_C(0x00000008) /* [bit(3)] Bit Manipulation Instruction Set 1. */
 #define CPUID_7B_HLE             __UINT32_C(0x00000010) /* [bit(4)] Transactional Synchronization Extensions. */
 #define CPUID_7B_AVX2            __UINT32_C(0x00000020) /* [bit(5)] Advanced Vector Extensions 2. */
-/*      CPUID_7B_                __UINT32_C(0x00000040) /* [bit(6)] RESERVED. */
+/*      CPUID_7B_                __UINT32_C(0x00000040)  * [bit(6)] RESERVED. */
 #define CPUID_7B_SMEP            __UINT32_C(0x00000080) /* [bit(7)] Supervisor-Mode Execution Prevention. */
 #define CPUID_7B_BMI2            __UINT32_C(0x00000100) /* [bit(8)] Bit Manipulation Instruction Set 2. */
 #define CPUID_7B_ERMS            __UINT32_C(0x00000200) /* [bit(9)] Enhanced REP MOVSB/STOSB. */
@@ -215,24 +215,24 @@
 #define CPUID_80000001C_XOP           __UINT32_C(0x00000800) /* [bit(11)] XOP instruction set. */
 #define CPUID_80000001C_SKINIT        __UINT32_C(0x00001000) /* [bit(12)] SKINIT/STGI instructions. */
 #define CPUID_80000001C_WDT           __UINT32_C(0x00002000) /* [bit(13)] Watchdog timer. */
-/*      CPUID_80000001C_              __UINT32_C(0x00004000) /* [bit(14)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x00004000)  * [bit(14)] RESERVED. */
 #define CPUID_80000001C_LWP           __UINT32_C(0x00008000) /* [bit(15)] Light Weight Profiling[23]. */
 #define CPUID_80000001C_FMA4          __UINT32_C(0x00010000) /* [bit(16)] 4 operands fused multiply-add. */
 #define CPUID_80000001C_TCE           __UINT32_C(0x00020000) /* [bit(17)] Translation Cache Extension. */
-/*      CPUID_80000001C_              __UINT32_C(0x00040000) /* [bit(18)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x00040000)  * [bit(18)] RESERVED. */
 #define CPUID_80000001C_NODEID_MSR    __UINT32_C(0x00080000) /* [bit(19)] NodeID MSR. */
-/*      CPUID_80000001C_              __UINT32_C(0x00100000) /* [bit(20)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x00100000)  * [bit(20)] RESERVED. */
 #define CPUID_80000001C_TBM           __UINT32_C(0x00200000) /* [bit(21)] Trailing Bit Manipulation. */
 #define CPUID_80000001C_TOPOEXT       __UINT32_C(0x00400000) /* [bit(22)] Topology Extensions. */
 #define CPUID_80000001C_PERFCTR_CORE  __UINT32_C(0x00800000) /* [bit(23)] Core performance counter extensions. */
 #define CPUID_80000001C_PERFCTR_NB    __UINT32_C(0x01000000) /* [bit(24)] NB performance counter extensions. */
-/*      CPUID_80000001C_              __UINT32_C(0x02000000) /* [bit(25)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x02000000)  * [bit(25)] RESERVED. */
 #define CPUID_80000001C_DBX           __UINT32_C(0x04000000) /* [bit(26)] Data breakpoint extensions. */
 #define CPUID_80000001C_PERFTSC       __UINT32_C(0x08000000) /* [bit(27)] Performance TSC. */
 #define CPUID_80000001C_PCX_L2I       __UINT32_C(0x10000000) /* [bit(28)] L2I perf counter extensions. */
-/*      CPUID_80000001C_              __UINT32_C(0x20000000) /* [bit(29)] RESERVED. */
-/*      CPUID_80000001C_              __UINT32_C(0x40000000) /* [bit(30)] RESERVED. */
-/*      CPUID_80000001C_              __UINT32_C(0x80000000) /* [bit(31)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x20000000)  * [bit(29)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x40000000)  * [bit(30)] RESERVED. */
+/*      CPUID_80000001C_              __UINT32_C(0x80000000)  * [bit(31)] RESERVED. */
 #define CPUID_80000001D_FPU           CPUID_1D_FPU           /* [bit(0)] Onboard x87 FPU. */
 #define CPUID_80000001D_VME           CPUID_1D_VME           /* [bit(1)] Virtual mode extensions (VIF). */
 #define CPUID_80000001D_DE            CPUID_1D_DE            /* [bit(2)] Debugging extensions (CR4 bit 3). */
@@ -243,7 +243,7 @@
 #define CPUID_80000001D_MCE           CPUID_1D_MCE           /* [bit(7)] Machine Check Exception. */
 #define CPUID_80000001D_CX8           CPUID_1D_CX8           /* [bit(8)] CMPXCHG8B (compare-and-swap) instruction. */
 #define CPUID_80000001D_APIC          CPUID_1D_APIC          /* [bit(9)] Onboard Advanced Programmable Interrupt Controller. */
-/*      CPUID_80000001D_              __UINT32_C(0x00000400) /* [bit(10)] RESERVED. */
+/*      CPUID_80000001D_              __UINT32_C(0x00000400)  * [bit(10)] RESERVED. */
 #define CPUID_80000001D_SYSCALL       __UINT32_C(0x00000800) /* [bit(11)] SYSCALL and SYSRET instructions. */
 #define CPUID_80000001D_MTRR          CPUID_1D_MTRR          /* [bit(12)] Memory Type Range Registers. */
 #define CPUID_80000001D_PGE           CPUID_1D_PGE           /* [bit(13)] Page Global Enable bit in CR4. */
@@ -251,17 +251,17 @@
 #define CPUID_80000001D_CMOV          CPUID_1D_CMOV          /* [bit(15)] Conditional move and FCMOV instructions. */
 #define CPUID_80000001D_PAT           CPUID_1D_PAT           /* [bit(16)] Page Attribute Table. */
 #define CPUID_80000001D_PSE36         CPUID_1D_PSE36         /* [bit(17)] 36-bit page size extension (for 4MiB pages). */
-/*      CPUID_80000001D_              __UINT32_C(0x00040000) /* [bit(18)] RESERVED. */
+/*      CPUID_80000001D_              __UINT32_C(0x00040000)  * [bit(18)] RESERVED. */
 #define CPUID_80000001D_MP            __UINT32_C(0x00080000) /* [bit(19)] Multiprocessor Capable. */
 #define CPUID_80000001D_NX            __UINT32_C(0x00100000) /* [bit(20)] NX bit. */
-/*      CPUID_80000001D_              __UINT32_C(0x00200000) /* [bit(21)] RESERVED. */
+/*      CPUID_80000001D_              __UINT32_C(0x00200000)  * [bit(21)] RESERVED. */
 #define CPUID_80000001D_MMXEXT        __UINT32_C(0x00400000) /* [bit(22)] Extended MMX. */
 #define CPUID_80000001D_MMX           CPUID_1D_MMX           /* [bit(23)] MMX instructions. */
 #define CPUID_80000001D_FXSR          CPUID_1D_FXSR          /* [bit(24)] FXSAVE, FXRSTOR instructions, CR4 bit 9. */
 #define CPUID_80000001D_FXSR_OPT      __UINT32_C(0x02000000) /* [bit(25)] FXSAVE/FXRSTOR optimizations. */
 #define CPUID_80000001D_PDPE1GB       __UINT32_C(0x04000000) /* [bit(26)] Gibabyte pages. */
 #define CPUID_80000001D_RDTSCP        __UINT32_C(0x08000000) /* [bit(27)] RDTSCP instruction. */
-/*      CPUID_80000001D_              __UINT32_C(0x10000000) /* [bit(28)] RESERVED. */
+/*      CPUID_80000001D_              __UINT32_C(0x10000000)  * [bit(28)] RESERVED. */
 #define CPUID_80000001D_LM            __UINT32_C(0x20000000) /* [bit(29)] Long mode. */
 #define CPUID_80000001D_3DNOWEXT      __UINT32_C(0x40000000) /* [bit(30)] Extended 3DNow. */
 #define CPUID_80000001D_3DNOW         __UINT32_C(0x80000000) /* [bit(31)] 3DNow!. */
