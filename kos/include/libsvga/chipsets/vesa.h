776555a2091fb4e573455150d5031a8eff528826
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2021-11-11 13:58:03 +0100

    List VESA video modes

--- kos/include/libsvga/chipsets/vesa.h
+++ kos/include/libsvga/chipsets/vesa.h
@@ -28,14 +28,16 @@
 #ifdef __CC__
 __DECL_BEGIN
 
+/* Based on info from (german): https://www.lowlevel.eu/wiki/VESA_BIOS_Extensions */
 
+
+/* Flags for `struct vbe_biosinfo::vbi_capabilities' */
 #define VBE_BIOSINFO_CAP_DACWIDTH 0x0001 /* clear: DAC is 6-bit; set: DAC is 6 or 8 bit. */
 #define VBE_BIOSINFO_CAP_NOVGACOM 0x0002 /* clear: VGA-compat; set: not VGA compat. */
 #define VBE_BIOSINFO_CAP_RAMDACOP 0x0004 /* ??? */
 
 /* VBE info structure */
 struct __ATTR_PACKED vbe_biosinfo {
-	/* https://www.lowlevel.eu/wiki/VESA_BIOS_Extensions */
 	char       vbi_signature[4];         /* == { 'V', 'E', 'S', 'A' } */
 	__uint16_t vbi_version;              /* Version number (0xff00: major; 0x00ff: minor) */
 	__uint16_t vbi_oem_string_off;       /* Far pointer to OEM string */
@@ -54,6 +56,87 @@ struct __ATTR_PACKED vbe_biosinfo {
 	__byte_t   vbi_reserved[222];        /* Reserved */
 	__byte_t   vbi_oemstrings[256];      /* Storage area for OEM strings. */
 };
+
+
+/* Flags for `struct vbe_modeinfo::vmi_attrib' */
+#define VBE_MODEINFO_ATTR_SUPPORTED 0x0001 /* Supported mode. (If not set, ignore this mode) */
+/*efine VBE_MODEINFO_ATTR_          0x0002  * ... */
+#define VBE_MODEINFO_ATTR_BIOSTTY   0x0004 /* BIOS supports TTY functions in this mode. */
+#define VBE_MODEINFO_ATTR_COLOR     0x0008 /* This mode supports colors. */
+#define VBE_MODEINFO_ATTR_GFX       0x0010 /* This is a graphics-mode (as opposed to text-mode) */
+#define VBE_MODEINFO_ATTR_NOVGACOM  0x0020 /* This mode isn't VGA compatible. */
+#define VBE_MODEINFO_ATTR_WINDOWED  0x0040 /* Memory from this mode is windowed (accessed as individual pages). */
+#define VBE_MODEINFO_ATTR_LFB       0x0080 /* Supports a linear frame buffer. */
+
+/* Flags for `struct vbe_modeinfo::vmi_wina_attr' / `struct vbe_modeinfo::vmi_winb_attr' */
+#define VBE_WINATTR_MAYRELOC 0x01 /* Page can be reallocated */
+#define VBE_WINATTR_MAYREAD  0x02 /* Page can be read from */
+#define VBE_WINATTR_MAYWRITE 0x04 /* Page can be written to */
+
+/* Values for `struct vbe_modeinfo::vmi_memmodel' */
+#define VBE_MEMMODEL_TEXT     0x00 /* Text mode */
+#define VBE_MEMMODEL_CGA      0x01 /* CGA graphics */
+#define VBE_MEMMODEL_HERCULES 0x02 /* Hercules graphics */
+#define VBE_MEMMODEL_PLANAR   0x03 /* Planar */
+#define VBE_MEMMODEL_Packed   0x04 /* Packed pixel */
+#define VBE_MEMMODEL_NC4_256  0x05 /* Non-chain 4, 256 color */
+#define VBE_MEMMODEL_DCOLOR   0x06 /* Direct color */
+#define VBE_MEMMODEL_YUV      0x07 /* YUV */
+
+/* Values for `struct vbe_modeinfo::vmi_dcolinfo' */
+#define VBE_DCOLINFO_RAMPMOD  0x01 /* Color ramp can be configured */
+#define VBE_DCOLINFO_RESAVAIL 0x02 /* Reserved bits (vmi_Xbits, vmi_Xshift) may be clobbered */
+
+/* VBE mode info structure */
+struct __ATTR_PACKED vbe_modeinfo {
+	__uint16_t vmi_attrib;            /* Mode attributes (set of `VBE_MODEINFO_ATTR_*') */
+	__uint8_t  vmi_wina_attr;         /* Window a attributes (set of `VBE_WINATTR_*') */
+	__uint8_t  vmi_winb_attr;         /* Window b attributes (set of `VBE_WINATTR_*') */
+	__uint16_t vmi_win_granularity;   /* Window granularity in Kb */
+	__uint16_t vmi_win_size;          /* Window size in Kb */
+	__uint16_t vmi_wina_segment;      /* Window a starting segment (real-mode-style) */
+	__uint16_t vmi_winb_segment;      /* Window b starting segment (real-mode-style) */
+	__uint16_t vmi_winfuncptr_off;    /* ??? Far pointer to window function */
+	__uint16_t vmi_winfuncptr_seg;    /* ??? *ditto* */
+	__uint16_t vmi_scanline;          /* Scanline size (in bytes) */
+	__uint16_t vmi_resx;              /* Resolution (in X) */
+	__uint16_t vmi_resy;              /* Resolution (in Y) */
+	__uint8_t  vmi_chrsizex;          /* Text-mode character size (in X) */
+	__uint8_t  vmi_chrsizey;          /* Text-mode character size (in Y) */
+	__uint8_t  vmi_numplanes;         /* Number of planes */
+	__uint8_t  vmi_bpp;               /* Bits per pixel (color-depth) */
+	__uint8_t  vmi_numbanks;          /* Number of banks */
+	__uint8_t  vmi_memmodel;          /* Memory model (oen of `VBE_MEMMODEL_*') */
+	__uint8_t  vmi_banksize;          /* Bask size in Kb */
+	__uint8_t  vmi_maximgplanes;      /* Max image plane number (== number of image planes -1) */
+	__uint8_t _vmi_res1;              /* ... */
+	__uint8_t  vmi_rbits;             /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] # of red color bits */
+	__uint8_t  vmi_rshift;            /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] Left-shift for red color bits */
+	__uint8_t  vmi_gbits;             /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] # of green color bits */
+	__uint8_t  vmi_gshift;            /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] Left-shift for green color bits */
+	__uint8_t  vmi_bbits;             /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] # of blue color bits */
+	__uint8_t  vmi_bshift;            /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] Left-shift for blue color bits */
+	__uint8_t  vmi_Xbits;             /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] # of reserved color bits */
+	__uint8_t  vmi_Xshift;            /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] Left-shift for reserved color bits */
+	__uint8_t  vmi_dcolinfo;          /* [valid_if(vmi_memmodel == VBE_MEMMODEL_DCOLOR)] Set of `VBE_DCOLINFO_*' */
+	__uint32_t vmi_lfbaddr;           /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Physical address of LFB buffer. */
+	__uint32_t vmi_offscrn_offs;      /* Start of off-screen video memory (TODO: Is this an offset, or a physical address?) */
+	__uint16_t vmi_offscrn_size;      /* Size of off-screen video memory */
+	__uint16_t vmi_lfbscanline;       /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Scanline size for LFB (in bytes) */
+	__uint8_t  vmi_bnknumimgpages;    /* ??? */
+	__uint8_t  vmi_linnumimgpages;    /* [valid_if(VBE_MODEINFO_ATTR_LFB)] ??? */
+	__uint8_t  vmi_linrbits;          /* [valid_if(VBE_MODEINFO_ATTR_LFB)] # of red color bits */
+	__uint8_t  vmi_linrshift;         /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Left-shift for red color bits */
+	__uint8_t  vmi_lingbits;          /* [valid_if(VBE_MODEINFO_ATTR_LFB)] # of green color bits */
+	__uint8_t  vmi_lingshift;         /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Left-shift for green color bits */
+	__uint8_t  vmi_linbbits;          /* [valid_if(VBE_MODEINFO_ATTR_LFB)] # of blue color bits */
+	__uint8_t  vmi_linbshift;         /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Left-shift for blue color bits */
+	__uint8_t  vmi_linXbits;          /* [valid_if(VBE_MODEINFO_ATTR_LFB)] # of reserved color bits */
+	__uint8_t  vmi_linXshift;         /* [valid_if(VBE_MODEINFO_ATTR_LFB)] Left-shift for reserved color bits */
+	__uint32_t vmi_maxpxclock;        /* ??? */
+	__byte_t  _vmi_res2[190];         /* ... */
+};
+
 
 __DECL_END
 #endif /* __CC__ */
--- kos/src/libsvga/cs-vesa.c
+++ kos/src/libsvga/cs-vesa.c
@@ -56,14 +56,16 @@ PRIVATE struct bios86_emulator vesa_emu;
 
 
 /* Check if the BIOS has VESA support. */
-PRIVATE struct vbe_biosinfo *CC vesa_testbios(void) {
+PRIVATE struct vbe_biosinfo *CC vesa_getbiosinfo(void) {
+#define BUFFER_OFFSET 0
 	struct vbe_biosinfo *result;
-	result = (struct vbe_biosinfo *)vesa_gpbase;
-	memset(&vesa_emu.b86e_vm.vr_regs, 0, sizeof(vesa_emu.b86e_vm.vr_regs));
+	result = (struct vbe_biosinfo *)(vesa_gpbase + BUFFER_OFFSET);
 	DBG_memset(result, 0xcc, sizeof(*result));
+	memset(&vesa_emu.b86e_vm.vr_regs, 0, sizeof(vesa_emu.b86e_vm.vr_regs));
 	memcpy(result->vbi_signature, "VBE2", 4);
 	vesa_emu.b86e_vm.vr_regs.vr_eax = 0x4f00;
-	vesa_emu.b86e_vm.vr_regs.vr_es  = VESA_GPBASE >> 4;
+	vesa_emu.b86e_vm.vr_regs.vr_es  = (VESA_GPBASE + BUFFER_OFFSET) >> 4;
+	vesa_emu.b86e_vm.vr_regs.vr_edi = (VESA_GPBASE + BUFFER_OFFSET) & 0xf;
 	if (!bios86_emulator_int(&vesa_emu, 0x10))
 		return NULL;
 	if ((vesa_emu.b86e_vm.vr_regs.vr_eax & 0xffff) != 0x4f)
@@ -71,6 +73,24 @@ PRIVATE struct vbe_biosinfo *CC vesa_testbios(void) {
 	if (memcmp(result->vbi_signature, "VESA", 4) != 0)
 		return NULL;
 	return result;
+#undef BUFFER_OFFSET
+}
+
+/* Check if the BIOS has VESA support. */
+PRIVATE struct vbe_modeinfo *CC vesa_getmodeinfo(uint16_t mode) {
+#define BUFFER_OFFSET sizeof(struct vbe_biosinfo)
+	struct vbe_modeinfo *result;
+	result = (struct vbe_modeinfo *)(vesa_gpbase + BUFFER_OFFSET);
+	DBG_memset(result, 0xcc, sizeof(*result));
+	memset(&vesa_emu.b86e_vm.vr_regs, 0, sizeof(vesa_emu.b86e_vm.vr_regs));
+	vesa_emu.b86e_vm.vr_regs.vr_eax = 0x4f01;
+	vesa_emu.b86e_vm.vr_regs.vr_ecx = mode;
+	vesa_emu.b86e_vm.vr_regs.vr_es  = (VESA_GPBASE + BUFFER_OFFSET) >> 4;
+	vesa_emu.b86e_vm.vr_regs.vr_edi = (VESA_GPBASE + BUFFER_OFFSET) & 0xf;
+	if (!bios86_emulator_int(&vesa_emu, 0x10))
+		return NULL;
+	return result;
+#undef BUFFER_OFFSET
 }
 
 
@@ -78,7 +98,7 @@ INTERN void vesa_init(void) {
 	struct vbe_biosinfo *vbi;
 	u16 *modelist, mode;
 	bios86_emulator_init(&vesa_emu);
-	vbi = vesa_testbios();
+	vbi = vesa_getbiosinfo();
 	if (!vbi) {
 		printk(KERN_DEBUG "[vesa] No BIOS support\n");
 		bios86_emulator_fini(&vesa_emu);
@@ -92,8 +112,15 @@ INTERN void vesa_init(void) {
 	printk(KERN_INFO "[vesa] oem_product_rev:  %q\n", (char *)vesa_farptr(vbi->vbi_oem_product_rev_seg, vbi->vbi_oem_product_rev_off));
 	modelist = (u16 *)vesa_farptr(vbi->vbi_vidmodelist_seg, vbi->vbi_vidmodelist_off);
 	for (; (mode = *modelist) != 0xffff; ++modelist) {
-		printk(KERN_INFO "[vesa] mode: %" PRIu16 "\n", mode);
-		/* TODO */
+		struct vbe_modeinfo *mi = vesa_getmodeinfo(mode);
+		if unlikely(!mi) {
+			printk(KERN_ERR "[vesa] BAD MODE %" PRIu16 "\n", mode);
+			continue;
+		}
+		if (!(mi->vmi_attrib & VBE_MODEINFO_ATTR_SUPPORTED))
+			continue;
+		printk(KERN_INFO "[vesa] mode %" PRIu16 ": %" PRIu16 "x%" PRIu16 "x%" PRIu8 "\n",
+		       mode, mi->vmi_resx, mi->vmi_resy, mi->vmi_bpp);
 	}
 
 	/* TODO */
