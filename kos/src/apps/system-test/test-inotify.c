7079eea984b1f29b36343886b2799f35b4e19442
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2022-04-04 19:33:38 +0200

    Add a system test for inotify
    Note that the fill-cached-directory-files-on-add-watch part still hasn't been implemented, though at this point that's pretty much the only thing that's still missing.

---
+++ kos/src/apps/system-test/test-inotify.c
@@ -0,0 +1,140 @@
+/* Copyright (c) 2019-2022 Griefer@Work                                       *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement (see the following) in the product     *
+ *    documentation is required:                                              *
+ *    Portions Copyright (c) 2019-2022 Griefer@Work                           *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ */
+#ifndef GUARD_APPS_SYSTEM_TEST_TEST_INOTIFY_C
+#define GUARD_APPS_SYSTEM_TEST_TEST_INOTIFY_C 1
+#define _KOS_SOURCE 1
+#undef NDEBUG
+
+#include <hybrid/compiler.h>
+
+#include <kos/types.h>
+#include <sys/inotify.h>
+#include <sys/stat.h>
+#include <system-test/ctest.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdalign.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+
+DECL_BEGIN
+
+alignas(alignof(struct inotify_event))
+PRIVATE byte_t _inotify_buf[offsetof(struct inotify_event, name) +
+                            256 * sizeof(char)];
+#define inotify_buf     ((struct inotify_event *)_inotify_buf)
+#define INOTIFY_BUFSIZE sizeof(_inotify_buf)
+
+PRIVATE void assert_inotify_event(fd_t notify_fd, watchfd_t wd, uint32_t mask, char const *name) {
+	ssize_t size, exp_size;
+	size     = read(notify_fd, inotify_buf, INOTIFY_BUFSIZE);
+	exp_size = offsetof(struct inotify_event, name);
+	if (name)
+		exp_size += (strlen(name) + 1) * sizeof(char);
+	EQss(size, exp_size);
+	if (name) {
+		assertf(strcmp(inotify_buf->name, name) == 0,
+		        "%q != %q", inotify_buf->name, name);
+		EQu32(inotify_buf->len, strlen(name) + 1);
+	} else {
+		EQu32(inotify_buf->len, 0);
+	}
+	EQu32(inotify_buf->mask, mask);
+	EQd32(inotify_buf->wd, wd);
+}
+
+PRIVATE void assert_inotify_empty(fd_t notify_fd) {
+	errno = 0;
+	EQss(-1, read(notify_fd, inotify_buf, INOTIFY_BUFSIZE));
+	EQd(errno, EWOULDBLOCK);
+}
+
+DEFINE_TEST(inotify) {
+	fd_t notify_fd;
+	watchfd_t wfd;
+	fd_t fd;
+	LEd(0, (notify_fd = inotify_init1(IN_CLOEXEC | IN_NONBLOCK)));
+	assert_inotify_empty(notify_fd);
+	LEd(0, (wfd = inotify_add_watch(notify_fd, "/tmp", IN_ALL_EVENTS | IN_ONLYDIR)));
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_OPEN' */
+	LEd(0, (fd = open("/tmp", O_RDONLY)));
+	assert_inotify_event(notify_fd, wfd, IN_OPEN | IN_ISDIR, NULL);
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_CLOSE_NOWRITE' */
+	EQd(0, close(fd));
+	assert_inotify_event(notify_fd, wfd, IN_CLOSE_NOWRITE | IN_ISDIR, NULL);
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_CREATE' */
+	LEd(0, (fd = open("/tmp/testfile", O_RDWR | O_CREAT, 0644)));
+	assert_inotify_event(notify_fd, wfd, IN_CREATE, "testfile");
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_ATTRIB' */
+	EQd(0, fchmod(fd, 0777));
+	assert_inotify_event(notify_fd, wfd, IN_ATTRIB, "testfile");
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_MODIFY' */
+	EQss(7, write(fd, "Content", 7));
+	assert_inotify_event(notify_fd, wfd, IN_MODIFY, "testfile");
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_CLOSE_WRITE' */
+	EQd(0, close(fd));
+	assert_inotify_event(notify_fd, wfd, IN_CLOSE_NOWRITE, "testfile"); /* FIXME: This should be `IN_CLOSE_WRITE' */
+	assert_inotify_empty(notify_fd);
+
+	/* Test: `IN_DELETE' */
+	EQd(0, unlink("/tmp/testfile"));
+	assert_inotify_event(notify_fd, wfd, IN_ATTRIB, "testfile"); /* Because `st_nlink' was changed (same thing happens on linux, too!) */
+	assert_inotify_event(notify_fd, wfd, IN_DELETE, "testfile");
+	assert_inotify_empty(notify_fd);
+
+
+	/* Cleanup (and also test invocation of `inotify_rm_watch') */
+	EQd(0, inotify_rm_watch(notify_fd, wfd));
+	assert_inotify_event(notify_fd, wfd, IN_IGNORED | IN_ISDIR, NULL); /* Triggered by `inotify_rm_watch(2)' */
+	assert_inotify_empty(notify_fd);
+	LEd(0, (fd = open("/tmp", O_RDONLY)));
+	assert_inotify_empty(notify_fd);
+	EQd(0, close(fd));
+	assert_inotify_empty(notify_fd);
+	EQd(0, close(notify_fd));
+
+	/* Also test closing a notifyfd without first removing watches, as well as with pending events */
+	LEd(0, (notify_fd = inotify_init1(IN_CLOEXEC | IN_NONBLOCK)));
+	assert_inotify_empty(notify_fd);
+	LEd(0, (wfd = inotify_add_watch(notify_fd, "/tmp", IN_ALL_EVENTS | IN_ONLYDIR)));
+	LEd(0, (fd = open("/tmp", O_RDONLY))); /* This generates `IN_OPEN', but we won't consume that event! */
+	EQd(0, close(fd));
+	EQd(0, close(notify_fd));
+
+}
+
+DECL_END
+
+#endif /* !GUARD_APPS_SYSTEM_TEST_TEST_INOTIFY_C */
