dfbea1d786c6b5e9938c62f0f237fc4b402f045d
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2022-08-13 16:34:40 +0200

    Implement `libc_handle(3)' for ARM

--- kos/src/libc/hybrid/arch/arm/config.h
+++ kos/src/libc/hybrid/arch/arm/config.h
@@ -20,8 +20,52 @@
 #ifndef GUARD_LIBC_HYBRID_ARCH_ARM_CONFIG_H
 #define GUARD_LIBC_HYBRID_ARCH_ARM_CONFIG_H 1
 
+#include <__stdinc.h>
+
 /* Libc functions implemented  by arch-specific, optimized  functions.
  * Defining these macros causes libc's auto/user-system to not provide
  * the associated functions. */
+
+
+#ifndef __KERNEL__
+/* void ASMCALL libc_sig_restore(void); */
+#define LIBC_ARCH_HAVE_SIG_RESTORE 1
+
+/* void *LIBCCALL libc_handle(void); */
+#define LIBC_ARCH_HAVE_LIBC_HANDLE 1
+
+#ifdef __CC__
+#include <bits/types.h>
+
+DECL_BEGIN
+
+#define libc_handle libc_handle
+LOCAL ATTR_CONST ATTR_RETNONNULL WUNUSED void *
+NOTHROW(libc_handle)(void) {
+	void *result;
+	/* Sadly, there is no way to trick gcc into emitting
+	 * our custom literal as part of its literal pool :(
+	 *
+	 * And trying to encode it as `ldr %0, =current(tlsgd)' also break. */
+	__asm__("ldr    %0, 992f\n"
+	        "991:\t"
+	        "add    %0, pc, %0\n\t"
+	        "b      993f\n"
+	        "992:\t"
+	        ".word current(tlsgd) + (. - 991b - 8)\n"
+	        "993:"
+	        : "=r" (result));
+	return result;
+}
+
+DECL_END
+#endif /* __CC__ */
+
+#define LIBC_ARCH_HAVE_GETCONTEXT  1
+#define LIBC_ARCH_HAVE_SETCONTEXT  1
+#define LIBC_ARCH_HAVE_SWAPCONTEXT 1
+#define LIBC_ARCH_HAVE_MAKECONTEXT 1
+#endif /* !__KERNEL__ */
+
 
 #endif /* !GUARD_LIBC_HYBRID_ARCH_ARM_CONFIG_H */
