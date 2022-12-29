1f8070ac33bd7306e332f491cbcb70f8f41ea043
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2022-12-29 15:00:12 +0100

    Add support for Hurd's `<spin-lock.h>` header

---
+++ kos/include/spin-lock.h
@@ -0,0 +1,57 @@
+/* HASH CRC-32:0x28c7363d */
+/* Copyright (c) 2019-2022 Griefer@Work                                       *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement (see the following) in the product     *
+ *    documentation is required:                                              *
+ *    Portions Copyright (c) 2019-2022 Griefer@Work                           *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ */
+/* (#) Portability: GNU C Library (/mach/spinlock.h) */
+/* (#) Portability: GNU Hurd      (/usr/include/spinlock.h) */
+#ifndef _SPIN_LOCK_H
+#define _SPIN_LOCK_H 1
+
+#include "__stdinc.h"
+#include "__crt.h"
+
+#ifdef __COMPILER_HAVE_PRAGMA_GCC_SYSTEM_HEADER
+#pragma GCC system_header
+#endif /* __COMPILER_HAVE_PRAGMA_GCC_SYSTEM_HEADER */
+
+#include <lock-intern.h>
+
+#ifdef __CC__
+__SYSDECL_BEGIN
+
+typedef __spin_lock_t spin_lock_t;
+#define SPIN_LOCK_INITIALIZER __SPIN_LOCK_INITIALIZER
+
+#define spin_lock_init(lock) __spin_lock_init(lock)
+#define spin_lock(lock) __spin_lock(lock)
+#include <kos/sched/shared-lock.h>
+#if defined(__CRT_HAVE___spin_try_lock) || defined(__shared_lock_tryacquire)
+#define spin_try_lock(lock) __spin_try_lock(lock)
+#endif /* __CRT_HAVE___spin_try_lock || __shared_lock_tryacquire */
+#if defined(__CRT_HAVE___spin_unlock) || defined(__shared_lock_release)
+#define spin_unlock(lock) __spin_unlock(lock)
+#endif /* __CRT_HAVE___spin_unlock || __shared_lock_release */
+#if defined(__CRT_HAVE___spin_lock_locked) || defined(__shared_lock_acquired)
+#define spin_lock_locked(lock) __spin_lock_locked(lock)
+#endif /* __CRT_HAVE___spin_lock_locked || __shared_lock_acquired */
+
+__SYSDECL_END
+#endif /* __CC__ */
+
+#endif /* !_SPIN_LOCK_H */
---
+++ kos/src/libc/magic/spin-lock.c
@@ -0,0 +1,60 @@
+/* Copyright (c) 2019-2022 Griefer@Work                                       *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement (see the following) in the product     *
+ *    documentation is required:                                              *
+ *    Portions Copyright (c) 2019-2022 Griefer@Work                           *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ */
+%(c_prefix){
+/* (#) Portability: GNU C Library (/mach/spinlock.h) */
+/* (#) Portability: GNU Hurd      (/usr/include/spinlock.h) */
+}
+
+%[default:section(".text.crt{|.dos}.compat.hurd.futex")]
+%[default:nodos]
+
+%{
+#include <lock-intern.h>
+
+#ifdef __CC__
+__SYSDECL_BEGIN
+
+typedef __spin_lock_t spin_lock_t;
+#define SPIN_LOCK_INITIALIZER __SPIN_LOCK_INITIALIZER
+
+}
+
+%[insert:pp_if($has_function(__spin_lock_init))]
+%#define spin_lock_init(lock) __spin_lock_init(lock)
+%[insert:pp_endif]
+%[insert:pp_if($has_function(__spin_lock))]
+%#define spin_lock(lock) __spin_lock(lock)
+%[insert:pp_endif]
+%[insert:pp_if($has_function(__spin_try_lock))]
+%#define spin_try_lock(lock) __spin_try_lock(lock)
+%[insert:pp_endif]
+%[insert:pp_if($has_function(__spin_unlock))]
+%#define spin_unlock(lock) __spin_unlock(lock)
+%[insert:pp_endif]
+%[insert:pp_if($has_function(__spin_lock_locked))]
+%#define spin_lock_locked(lock) __spin_lock_locked(lock)
+%[insert:pp_endif]
+
+%{
+
+__SYSDECL_END
+#endif /* __CC__ */
+
+}
