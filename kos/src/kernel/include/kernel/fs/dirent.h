c7c527637fb8675f46b9936c0d65bc80dd7c4279
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2023-03-18 10:22:46 +0100

    Minor improvement to fnode sub-type detection
    Skip an unnecessary check in `fnode_isblkroot()`

--- kos/src/kernel/include/kernel/fs/dirent.h
+++ kos/src/kernel/include/kernel/fs/dirent.h
@@ -70,7 +70,7 @@ struct fdirent {
 	ino_t                                   fd_ino;      /* [const][valid_if(fd_ops->fdo_getino == NULL)] INode number of this directory
 	                                                      * entry. WARNING: This  value becomes  invalid if the  entry/file is  deleted! */
 	uintptr_t                               fd_hash;     /* [const][== fdirent_hash(fd_name, fd_namelen)] Hash of this directory entry. */
-	u16                                     fd_namelen;  /* [const][!0] Length of the directory entry name (in characters). */
+	u16                                     fd_namelen;  /* [const][!0] Length of the directory entry name (in c-chars). */
 	unsigned char                           fd_type;     /* [const] Directory entry type (one of `DT_*') */
 	COMPILER_FLEXIBLE_ARRAY(/*utf-8*/ char, fd_name);    /* [const][fd_namelen] Directory entry name. (NUL-terminated) */
 };
