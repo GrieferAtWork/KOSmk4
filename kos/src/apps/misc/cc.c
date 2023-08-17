52679825b27a86ab8cd4f1c0f0a1206d20e67d42
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2023-08-17 11:22:12 +0200

    Print how much was freed in `cc(1)`

--- kos/src/apps/misc/cc.c
+++ kos/src/apps/misc/cc.c
@@ -25,14 +25,55 @@ opt.removeif([](e) -> e.startswith("-fstack-protector"));
 #define GUARD_APPS_MISC_CC_C 1
 #define __CRT_FREESTANDING
 
+#include <hybrid/compiler.h>
+
 #include <kos/ksysctl.h>
 #include <kos/syscalls.h>
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void _start(void) {
-	sys_Xksysctl(KSYSCTL_SYSTEM_CLEARCACHES, NULL);
+	char buf[128], *p = COMPILER_ENDOF(buf);
+	size_t count, extra;
+	count = sys_Xksysctl(KSYSCTL_SYSTEM_CLEARCACHES, NULL);
+	*--p = '\n';
+	*--p = 'B';
+	if (count >= (1024 * 1024)) {
+		*--p = 'i';
+		*--p = 'M';
+		extra = count % (1024 * 1024);
+		extra *= 100;
+		extra /= (1024 * 1024);
+		count /= (1024 * 1024);
+	} else if (count >= 1024) {
+		*--p = 'i';
+		*--p = 'K';
+		extra = count % 1024;
+		extra *= 100;
+		extra /= 1024;
+		count /= 1024;
+	} else {
+		extra = 0;
+	}
+	if (extra) {
+		*--p = '0' + (extra % 10);
+		*--p = '0' + (extra / 10);
+		*--p = '.';
+	}
+	do {
+		*--p = '0' + (count % 10);
+		count /= 10;
+	} while (count);
+	*--p = ' ';
+	*--p = ':';
+	*--p = 'd';
+	*--p = 'e';
+	*--p = 'e';
+	*--p = 'r';
+	*--p = 'f';
+	sys_Xwrite(STDOUT_FILENO, p, (size_t)(COMPILER_ENDOF(buf) - p));
 	sys_Xexit(EXIT_SUCCESS);
 }
 
