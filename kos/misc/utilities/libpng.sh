321007c8800408bdddf75f8d0de2d43585ddc65a
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2025-06-19 21:32:39 +0200

    Include the APNG patch in libpng

--- kos/misc/utilities/libpng.sh
+++ kos/misc/utilities/libpng.sh
@@ -20,10 +20,15 @@
 
 require_utility libzlib "$PKG_CONFIG_PATH/zlib.pc"
 
-PACKAGE_URL="https://sourceforge.net/projects/libpng/files/libpng16/1.6.49/libpng-1.6.49.tar.gz/download"
-PACKAGE_URL_FILENAME="libpng-1.6.49.tar.gz"
-PACKAGE_NAME="libpng-1.6.49"
-VERSION="16"
+
+LIBPNG_VERSION_SO="16"
+LIBPNG_VERSION="1.6.49"
+APNG_PATCH="https://sourceforge.net/projects/libpng-apng/files/libpng16/${LIBPNG_VERSION}/libpng-${LIBPNG_VERSION}}-apng.patch.gz/download"
+APNG_PATCH_FILENAME="libpng-${LIBPNG_VERSION}}-apng.patch"
+APNG_PATCH_FILENAME_GZ="${APNG_PATCH_FILENAME}.gz"
+PACKAGE_URL="https://sourceforge.net/projects/libpng/files/libpng16/${LIBPNG_VERSION}/libpng-${LIBPNG_VERSION}}.tar.gz/download"
+PACKAGE_URL_FILENAME="libpng-${LIBPNG_VERSION}}.tar.gz"
+PACKAGE_NAME="libpng-${LIBPNG_VERSION}"
 
 CONFIGURE=(
 	"--enable-hardware-optimizations"
@@ -31,9 +36,27 @@ CONFIGURE=(
 
 INSTALL_SKIP="
 /bin/libpng-config
-/bin/libpng${VERSION}-config
-/usr/include/libpng${VERSION}/pnglibconf.h
+/bin/libpng${LIBPNG_VERSION_SO}-config
+/usr/include/libpng${LIBPNG_VERSION_SO}/pnglibconf.h
 "
+
+libpng_apply_apng_patch() {
+	if ! [ -f "$SRCPATH/.apng-patched" ]; then
+		cmd cd "$KOS_ROOT/binutils/src"
+		download_file "$APNG_PATCH_FILENAME_GZ" "$APNG_PATCH"
+		mkdir -p ".apng-tmp"
+		cmd cp "$APNG_PATCH_FILENAME_GZ" ".apng-tmp"
+		cmd cd ".apng-tmp"
+		cmd gunzip "$APNG_PATCH_FILENAME_GZ"
+		apply_patch "$SRCPATH" "$APNG_PATCH_FILENAME"
+		cmd cd "$KOS_ROOT/binutils/src"
+		cmd rm -r ".apng-tmp"
+		> "$SRCPATH/.apng-patched"
+		cmd cd "$SRCPATH"
+	fi
+}
+GM_HOOK_BEFORE_CONFARGS=libpng_apply_apng_patch
+
 
 # Automatically build+install using autoconf
 . "$KOS_MISC/utilities/misc/gnu_make.sh"
@@ -42,10 +65,10 @@ INSTALL_SKIP="
 install_symlink_nodisk /$TARGET_LIBPATH/libpng.so libpng16.so.16
 
 # Install headers in a more portable way
-install_rawfile "$KOS_ROOT/kos/include/$TARGET_INCPATH/libpng${VERSION}/pnglibconf.h" "$DESTDIR/usr/include/libpng${VERSION}/pnglibconf.h"
-install_rawfile_stdin "$KOS_ROOT/kos/include/libpng${VERSION}/pnglibconf.h" <<EOF
+install_rawfile "$KOS_ROOT/kos/include/$TARGET_INCPATH/libpng${LIBPNG_VERSION_SO}/pnglibconf.h" "$DESTDIR/usr/include/libpng${LIBPNG_VERSION_SO}/pnglibconf.h"
+install_rawfile_stdin "$KOS_ROOT/kos/include/libpng${LIBPNG_VERSION_SO}/pnglibconf.h" <<EOF
 #ifndef PNGLCONF_H
-#include <libpng${VERSION}/pnglibconf.h> /* arch-specific header */
+#include <libpng${LIBPNG_VERSION_SO}/pnglibconf.h> /* arch-specific header */
 #ifndef PNGLCONF_H
 #define PNGLCONF_H
 #endif /* !PNGLCONF_H */
@@ -59,12 +82,12 @@ EOF
 }
 
 cmd mkdir -p "$KOS_ROOT/kos/include/libpng"
-install_proxy_c_header "$KOS_ROOT/kos/include/libpng/png.h"        "../libpng${VERSION}/png.h"
-install_proxy_c_header "$KOS_ROOT/kos/include/libpng/pngconf.h"    "../libpng${VERSION}/pngconf.h"
-install_proxy_c_header "$KOS_ROOT/kos/include/libpng/pnglibconf.h" "../libpng${VERSION}/pnglibconf.h"
-install_proxy_c_header "$KOS_ROOT/kos/include/png.h"               "libpng${VERSION}/png.h"
-install_proxy_c_header "$KOS_ROOT/kos/include/pngconf.h"           "libpng${VERSION}/pngconf.h"
-install_proxy_c_header "$KOS_ROOT/kos/include/pnglibconf.h"        "libpng${VERSION}/pnglibconf.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/libpng/png.h"        "../libpng${LIBPNG_VERSION_SO}/png.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/libpng/pngconf.h"    "../libpng${LIBPNG_VERSION_SO}/pngconf.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/libpng/pnglibconf.h" "../libpng${LIBPNG_VERSION_SO}/pnglibconf.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/png.h"               "libpng${LIBPNG_VERSION_SO}/png.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/pngconf.h"           "libpng${LIBPNG_VERSION_SO}/pngconf.h"
+install_proxy_c_header "$KOS_ROOT/kos/include/pnglibconf.h"        "libpng${LIBPNG_VERSION_SO}/pnglibconf.h"
 
 
 # Install the PKG_CONFIG file
@@ -72,12 +95,12 @@ install_rawfile_stdin "$PKG_CONFIG_PATH/libpng.pc" <<EOF
 prefix=/
 exec_prefix=/
 libdir=$KOS_ROOT/bin/$TARGET_NAME-kos/$TARGET_LIBPATH
-includedir=$KOS_ROOT/kos/include/libpng$SO_VERSION_MAJOR
+includedir=$KOS_ROOT/kos/include/libpng${LIBPNG_VERSION_SO}
 
 Name: libpng
 Description: Loads and saves PNG files
-Version: $VERSION
+Version: ${LIBPNG_VERSION_SO}
 Requires: zlib
-Libs: -lpng$SO_VERSION_MAJOR
+Libs: -lpng${LIBPNG_VERSION_SO}
 Cflags:
 EOF
