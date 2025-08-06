22c4bd249af94e3440a96fd89d6ff6bc060d48a6
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2025-08-06 13:51:15 +0200

    Fix a GCC incompatibility in `dwarf_decode_pointer()`

--- kos/src/libunwind/dwarf.c
+++ kos/src/libunwind/dwarf.c
@@ -51,7 +51,7 @@ NOTHROW_NCX(CC libuw_dwarf_decode_sleb128)(NCX byte_t const **__restrict p_reade
 	NCX byte_t const *reader = *p_reader;
 	byte_t byte;
 	dwarf_sleb128_t result = 0;
-	shift_t shift          = 0;
+	shift_t shift = 0;
 	for (;;) {
 		byte = *reader++;
 		result |= ((byte & 0x7f) << shift);
@@ -71,7 +71,7 @@ NOTHROW_NCX(CC libuw_dwarf_decode_uleb128)(NCX byte_t const **__restrict p_reade
 	NCX byte_t const *reader = *p_reader;
 	byte_t byte;
 	dwarf_uleb128_t result = 0;
-	shift_t shift          = 0;
+	shift_t shift = 0;
 	for (;;) {
 		byte    = *reader++;
 		result |= ((byte & 0x7f) << shift);
@@ -86,8 +86,7 @@ NOTHROW_NCX(CC libuw_dwarf_decode_uleb128)(NCX byte_t const **__restrict p_reade
 
 PRIVATE NONNULL((1)) NCX byte_t *
 NOTHROW_NCX(CC calculate_tbase)(NCX void const *modptr) {
-	void *result;
-	result = module_fromaddr_nx(modptr);
+	void *result = module_fromaddr_nx(modptr);
 	if (result != NULL)
 		result = (void *)module_get_tbase((module_t *)result);
 	return (byte_t *)result;
@@ -95,8 +94,7 @@ NOTHROW_NCX(CC calculate_tbase)(NCX void const *modptr) {
 
 PRIVATE NONNULL((1)) NCX byte_t *
 NOTHROW_NCX(CC calculate_dbase)(NCX void const *modptr) {
-	void *result;
-	result = module_fromaddr_nx(modptr);
+	void *result = module_fromaddr_nx(modptr);
 	if (result != NULL)
 		result = (void *)module_get_dbase((module_t *)result);
 	return (byte_t *)result;
@@ -107,6 +105,7 @@ INTERN NONNULL((1)) NCX byte_t *
 NOTHROW_NCX(CC libuw_dwarf_decode_pointer)(NCX byte_t const **__restrict p_reader,
                                            uint8_t encoding, uint8_t addrsize,
                                            struct unwind_bases *dw_bases) {
+	uintptr_t offset;
 	NCX byte_t *result;
 	NCX byte_t const *reader = *p_reader;
 
@@ -167,84 +166,142 @@ NOTHROW_NCX(CC libuw_dwarf_decode_pointer)(NCX byte_t const **__restrict p_reade
 
 	case DW_EH_PE_absptr:
 		if (addrsize >= sizeof(uintptr_t)) {
-			result += UNALIGNED_GET((uintptr_t const *)reader);
+			offset = UNALIGNED_GET((uintptr_t const *)reader);
 #if __SIZEOF_POINTER__ > 4
 		} else if (addrsize >= 4) {
-			result += UNALIGNED_GET32(reader);
+			offset = UNALIGNED_GET32(reader);
 #endif /* __SIZEOF_POINTER__ > 4 */
 		} else if (addrsize >= 2) {
-			result += UNALIGNED_GET16(reader);
+			offset = UNALIGNED_GET16(reader);
 		} else if (addrsize >= 1) {
-			result += UNALIGNED_GET8(reader);
+			offset = UNALIGNED_GET8(reader);
 		}
 		reader += addrsize;
 		break;
 
 	case DW_EH_PE_udata2:
-		result += (uintptr_t)UNALIGNED_GET16(reader);
+		offset = (uintptr_t)UNALIGNED_GET16(reader);
 		reader += 2;
 		break;
 
 	case DW_EH_PE_udata4:
-		result += (uintptr_t)UNALIGNED_GET32(reader);
+		offset = (uintptr_t)UNALIGNED_GET32(reader);
 		reader += 4;
 		break;
 
 	case DW_EH_PE_udata8:
 #if __SIZEOF_POINTER__ > 4
-		result += (uintptr_t)UNALIGNED_GET64(reader);
+		offset = (uintptr_t)UNALIGNED_GET64(reader);
 #elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-		result += (uintptr_t)UNALIGNED_GET32(reader);
+		offset = (uintptr_t)UNALIGNED_GET32(reader);
 #else /* ... */
-		result += (uintptr_t)UNALIGNED_GET32(reader + 4);
+		offset = (uintptr_t)UNALIGNED_GET32(reader + 4);
 #endif /* !... */
 		reader += 8;
 		break;
 
 	case DW_EH_PE_sdata2:
-		result += (uintptr_t)(intptr_t)(int16_t)UNALIGNED_GET16(reader);
+		offset = (uintptr_t)(intptr_t)(int16_t)UNALIGNED_GET16(reader);
 		reader += 2;
 		break;
 
 	case DW_EH_PE_sdata4:
-		result += (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader);
+		offset = (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader);
 		reader += 4;
 		break;
 
 	case DW_EH_PE_sdata8:
 #if __SIZEOF_POINTER__ > 4
-		result += (uintptr_t)(intptr_t)(int64_t)UNALIGNED_GET64(reader);
+		offset = (uintptr_t)(intptr_t)(int64_t)UNALIGNED_GET64(reader);
 #elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-		result += (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader);
+		offset = (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader);
 #else /* ... */
-		result += (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader + 4);
+		offset = (uintptr_t)(intptr_t)(int32_t)UNALIGNED_GET32(reader + 4);
 #endif /* !... */
 		reader += 8;
 		break;
 
 	case DW_EH_PE_uleb128:
-		result += libuw_dwarf_decode_uleb128(&reader);
+		offset = libuw_dwarf_decode_uleb128(&reader);
 		break;
 
 	case DW_EH_PE_sleb128:
-		result += libuw_dwarf_decode_sleb128(&reader);
+		offset = libuw_dwarf_decode_sleb128(&reader);
 		break;
 
 	default:
+		offset = 0;
 		reader += 1; /* ??? */
 		break;
 	}
-	if (encoding & DW_EH_PE_indirect) {
-		if (addrsize >= sizeof(uintptr_t)) {
-			result = (byte_t *)UNALIGNED_GET((uintptr_t const *)result);
+
+	/* Special ruling: "DW_EH_PE_indirect" is only applied when the preceding "offset" was non-zero!
+	 * This  behavior is **REQUIRED**  for .gcc_except_table to be  parsed properly, which generally
+	 * uses "encoding = 0x9b" (DW_EH_PE_indirect | DW_EH_PE_pcrel | DW_EH_PE_sdata4), and expects us
+	 * to  **NOT** dereference the produced pointer when  "DW_EH_PE_sdata4" equals 0. Looking at its
+	 * sample implementation of this function (then called `read_encoded_value()'), we can see this:
+	 *
+	 * .../gcc-12.1.0/libgcc/unwind-pe.h:210:
+	 * >> switch (encoding & 0x0f)                          /// [[[ HINT: This is our DW_EH_PE_OFF(encoding) ]]]
+	 * >> {
+	 * >> case DW_EH_PE_absptr:
+	 * >>   result = (_Unwind_Internal_Ptr) u->ptr;
+	 * >>   p += sizeof (void *);
+	 * >>   break;
+	 * >>
+	 * >> case DW_EH_PE_uleb128:
+	 * >>   {
+	 * >>     _uleb128_t tmp;
+	 * >>     p = read_uleb128 (p, &tmp);
+	 * >>     result = (_Unwind_Internal_Ptr) tmp;
+	 * >>   }
+	 * >>   break;
+	 * >>
+	 * >> case DW_EH_PE_sleb128:
+	 * >>   {
+	 * >>     _sleb128_t tmp;
+	 * >>     p = read_sleb128 (p, &tmp);
+	 * >>     result = (_Unwind_Internal_Ptr) tmp;
+	 * >>   }
+	 * >>   break;
+	 * >>
+	 * >> [...]
+	 * >>
+	 * >> default:
+	 * >>   __gxx_abort ();
+	 * >> }
+	 * >>
+	 * >> if (result != 0)                                  /// [[[ Deref only happens when the "offset"
+	 * >> {                                                 ///     (here-called "result") is non-zero ]]]
+	 * >>   [...]
+	 * >>   result += ((encoding & 0x70) == DW_EH_PE_pcrel
+	 * >>             ? (_Unwind_Internal_Ptr) u : base);
+	 * >>   if (encoding & DW_EH_PE_indirect)
+	 * >>     result = *(_Unwind_Internal_Ptr *) result;
+	 * >>   [...]
+	 * >> }
+	 * >> *val = result;                                    /// [[[ If the "offset" was 0, then the base is discarded
+	 * >> return p;                                         ///     and the returned pointer is also zero (aka. NULL) ]]]
+	 *
+	 * As  you can also  see, when the  offset _is_ 0, then
+	 * `*val' (our return value) must be 0 (aka. NULL), too
+	 */
+	if (offset == 0) {
+		result = NULL;
+	} else {
+		result += offset;
+		if (encoding & DW_EH_PE_indirect) {
+			if (addrsize >= sizeof(uintptr_t)) {
+				result = (byte_t *)UNALIGNED_GET((uintptr_t const *)result);
 #if __SIZEOF_POINTER__ > 4
-		} else if (addrsize >= 4) {
-			result = (byte_t *)(uintptr_t)UNALIGNED_GET32(result);
+			} else if (addrsize >= 4) {
+				result = (byte_t *)(uintptr_t)UNALIGNED_GET32(result);
 #endif /* __SIZEOF_POINTER__ > 4 */
-		} else if (addrsize >= 2) {
-			result = (byte_t *)(uintptr_t)UNALIGNED_GET16(result);
-		} else if (addrsize >= 1) {
-			result = (byte_t *)(uintptr_t)UNALIGNED_GET8(result);
+			} else if (addrsize >= 2) {
+				result = (byte_t *)(uintptr_t)UNALIGNED_GET16(result);
+			} else if (addrsize >= 1) {
+				result = (byte_t *)(uintptr_t)UNALIGNED_GET8(result);
+			}
 		}
 	}
 	*p_reader = reader;
